d27424f5c2e50d4bd585fb849c69b188a34d384d
Daniel Possenriede <possenriede@gmail.com>
2019-03-19 11:35:15 +0100

    test with rstudio-pandoc built with ghc 8.6.4

--- pandoc_tester.cmd
+++ pandoc_tester.cmd
@@ -1,26 +1,26 @@
-"C:/Program Files/Pandoc/pandoc" --version
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" --version
 :: HTML4
 :: 1. @znmeb's example
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none --self-contained
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none --self-contained
 :: 2. remove `--self-contained`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none
 :: 3. remove `--email-obfuscation none`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --self-contained
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --self-contained
 :: 4. add `--standalone`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none --self-contained --standalone
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none --self-contained --standalone
 :: 5. remove `--self-contained`, add `--standalone`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none --standalone
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html4 --output process_raw.html --email-obfuscation none --standalone
 
 :: HTML5
 :: 1. @znmeb's example
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none --self-contained
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none --self-contained
 :: 2. remove `--self-contained`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none
 :: 3. remove `--email-obfuscation none`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --self-contained
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --self-contained
 :: 4. add `--standalone`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none --self-contained --standalone
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none --self-contained --standalone
 :: 5. remove `--self-contained`, add `--standalone`
-"C:/Program Files/Pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none --standalone
+"C:/Users/daniel/Documents/rstudio-pandoc/pandoc" process_raw.md --from markdown --to html5 --output process_raw.html --email-obfuscation none --standalone
 
 pause
